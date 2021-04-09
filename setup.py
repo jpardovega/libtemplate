540b588ee3b299eed6914cf1d0989f4f5e6c960a
jpardovega <jpardovega@gmail.com>
2021-04-08 22:31:49 -0400

    Bump version: 0.0.6 → 0.0.7

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     use_scm_version={
         'local_scheme': 'dirty-tag',
         'write_to': 'src/libtemplate/_version.py',
-        'fallback_version': '0.0.6',
+        'fallback_version': '0.0.7',
     },
     description='Template for python packages generated with cookiecutter-pylibrary',
     long_description='%s\n%s' % (
